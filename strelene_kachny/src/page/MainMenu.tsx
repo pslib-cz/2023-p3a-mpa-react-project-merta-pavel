6543db19faf95fa6619a12ff5a3dce7abe11014a
Pavel Merta <pavel.merta05@gmail.com>
2024-05-13 14:41:26 +0200

    CSS

--- strelene_kachny/src/page/MainMenu.tsx
+++ strelene_kachny/src/page/MainMenu.tsx
@@ -5,7 +5,8 @@ import styles from './MainMenu.module.css';
 const MainMenu: React.FC = () => {
   return (
     <div className={styles.Menu}>
-    <h1 className={styles.Text}>Hlavní Menu</h1>
+      <h1 className={styles.Text}>Střelené Kachny</h1>
+    <h2 className={styles.Text}>Hlavní Menu</h2>
   <Link to="/settings">
     <button className={styles.Button}>
       Play Game
