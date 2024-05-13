da726572452357526d9b1816b04dd9151d526e04
Pavel Merta <pavel.merta05@gmail.com>
2024-05-13 15:02:22 +0200

    Router

--- strelene_kachny/src/App.tsx
+++ strelene_kachny/src/App.tsx
@@ -1,12 +1,24 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import MainMenu from './page/MainMenu';
 import './App.css';
 import Gameboard from './component/Gameboard';
 import Settings from './page/Settings';
+import { GameProvider } from './provider/provider.tsx'
 
-function App() {
+const App = () => {
 
-  const router = createBrowserRouter(
+  return (
+    <GameProvider>
+      <Router basename='/2023-p3a-mpa-react-project-merta-pavel'>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />,
+          <Route path="/settings" element={<Settings />} />,
+          <Route path="/component" element={<Gameboard />} />,
+        </Routes>
+      </Router>
+    </GameProvider>
+  )
+ /* const router = createBrowserRouter(
     createRoutesFromElements([
       <Route path="/" element={<MainMenu />} />,
       <Route path="/settings" element={<Settings />} />,
@@ -17,7 +29,7 @@ function App() {
     <>
       <RouterProvider router={router} />
     </>
-  )
+  )*/
 }
 
 export default App;
